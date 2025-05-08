40d61a241cf531881e6ff5884b93a057fcd35b68
Alexandre Carel <sobell13@gmail.com>
2025-05-08 14:18:08 +0200

    added script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+
+// Constants for the site logic
+const iframe = document.querySelector("#embed-frame");
+const figmaOrigin = "https://www.figma.com";
+
+// Messages to control the prototype
+function nextPage() {
+  iframe.contentWindow.postMessage(
+    {
+      type: "NAVIGATE_FORWARD"
+    },
+    figmaOrigin
+  );
+}
+
+function previousPage() {
+  iframe.contentWindow.postMessage(
+    {
+      type: "NAVIGATE_BACKWARD"
+    },
+    figmaOrigin
+  );
+}
+
+function restartPrototype() {
+  iframe.contentWindow.postMessage(
+    {
+      type: "RESTART"
+    },
+    figmaOrigin
+  );
+}
+
+const restartButton = document.querySelector("#restart");
+const nextButton = document.querySelector("#next");
+const prevButton = document.querySelector("#prev");
+
+
+// Logic to handle events from the prototype
+window.addEventListener("message", (event) => {
+  if (event.origin === figmaOrigin) {
+    if (event.data.type === "INITIAL_LOAD") {
+    }
+
+    if (event.data.type === "PRESENTED_NODE_CHANGED") {
+      const nodeId = event.data.data.presentedNodeId;
+    }
+  } else {
+    console.warn(
+      "Received message from an unexpected origin:",
+      event.origin
+    );
+  }
+});
